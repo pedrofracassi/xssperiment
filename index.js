473d1133949e7291987781b978350f17c5bdbd6b
Pedro Fracassi <pedroluizfracassi@gmail.com>
2018-12-04 13:33:14 -0300

    Force JavaScript

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const snekfetch = require('snekfetch')
 const express = require('express')
-const mime = require('mime-types')
-const path = require('path')
 const app = express()
 
 const port = process.env.PORT || 8080
@@ -9,7 +7,7 @@ const port = process.env.PORT || 8080
 app.get('/', async (req, res) => {
   try {
     const { body } = await snekfetch.get(process.env.GIST_URL)
-    res.set('content-type', mime.contentType(path.basename(process.env.GIST_URL))).send(body)
+    res.set('content-type', 'application/javascript; charset=utf-8').send(body)
   } catch (e) {
     console.error(e)
   }
