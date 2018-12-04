a5beba63428b1695b23e11768f63a0b0934f4033
Pedro Fracassi <pedroluizfracassi@gmail.com>
2018-12-04 13:27:29 -0300

    Initial code stuff

---
+++ index.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+
+const snekfetch = require('snekfetch')
+const express = require('express')
+const mime = require('mime-types')
+const path = require('path')
+const app = express()
+
+const port = process.env.PORT || 8080
+
+app.get('/', async (req, res) => {
+  try {
+    const { body } = await snekfetch.get(process.env.GIST_URL)
+    res.set('content-type', mime.contentType(path.basename(process.env.GIST_URL))).send(body)
+  } catch (e) {
+    console.error(e)
+  }
+})
+
+app.listen(port)
